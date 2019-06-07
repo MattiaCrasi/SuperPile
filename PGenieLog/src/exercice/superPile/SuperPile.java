6bb41d350faac61c75a02445bb09f872de32ef21
Mattia Crasi <mattia.crasi@rpn.ch>
2019-06-07 16:24:22 +0200

    classe Superpile, singleton, fonction empiler depiler taille

--- PGenieLog/src/exercice/superPile/SuperPile.java
+++ PGenieLog/src/exercice/superPile/SuperPile.java
@@ -1,33 +1,83 @@
 
 package exercice.superPile;
 
+import java.util.Stack;
 
-
-public class SuperPile
+/**
+ * singleton
+ */
+public class SuperPile extends Stack<Integer>
 	{
-	
+
 	/*------------------------------------------------------------------*\
 	|*							Constructeurs							*|
 	\*------------------------------------------------------------------*/
-	
+
+	private SuperPile()
+		{
+
+		}
+
 	/*------------------------------------------------------------------*\
 	|*							Methodes Public							*|
 	\*------------------------------------------------------------------*/
-	
+
+	public void empiler(int x)
+		{
+		push(x);
+		}
+
+	public int depiler()
+		{
+		if (0 == taille())
+			{
+			notifyElement();
+			}
+		return pop();
+		}
+
+	public int taille()
+		{
+		return size();
+		}
+
 	/*------------------------------*\
-	|*				Set				*|
+	|*			  Static			*|
 	\*------------------------------*/
-	
+
+	public static synchronized SuperPile getInstance()
+		{
+		if (instance == null)
+			{
+			System.out.println("Existe");
+			instance = new SuperPile();
+			}
+
+		return instance;
+		}
+
 	/*------------------------------*\
 	|*				Get				*|
 	\*------------------------------*/
-	
+
 	/*------------------------------------------------------------------*\
 	|*							Methodes Private						*|
 	\*------------------------------------------------------------------*/
-	
+
+	private void notifyElement()
+		{
+		System.out.println("La pile est vide");
+		}
+
 	/*------------------------------------------------------------------*\
 	|*							Attributs Private						*|
 	\*------------------------------------------------------------------*/
-	}
 
+	// Tools
+
+	/*------------------------------*\
+	|*			  Static			*|
+	\*------------------------------*/
+
+	private static SuperPile instance = null;
+	}
